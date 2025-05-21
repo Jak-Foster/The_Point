65ce43c5fdf1664c1c82434e22aae0ae6e39def8
JakFoster <jakfoster97@outlook.com>
2025-05-21 19:54:24 +0100

    Created Point class, initialised and displayed the value in the main method

---
+++ Point.cs
@@ -0,0 +1,20 @@
+﻿
+namespace The_Point
+{
+    internal class Point
+    {
+        private int X { get; }
+        private int Y { get; }
+        public Point (int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public Point ()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Point PointOne = new Point(2, 3);
+        Point PointTwo = new Point(-4, 0);
+        Console.WriteLine($"Point 1: ({PointOne.X}, {PointOne.Y})");
+        Console.WriteLine($"Point 2: ({PointTwo.X}, {PointTwo.Y})");
+    }
+}
